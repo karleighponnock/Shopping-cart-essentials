02052daae6963de5c7a1a8e7ab5ea7f10a618040
karleighponnock <karleighponnock@gmail.com>
2020-08-10 19:56:03 -0400

    key added to list

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -11,7 +11,7 @@ class Counter extends Component {
       <>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>{this.state.tags.map(tag => <li>{tag}</li>)}</ul>
+        <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
       </>
     );
   }
