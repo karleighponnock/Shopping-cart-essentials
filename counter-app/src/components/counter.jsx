f3e9a11fd1133695b870f3c3092a6f85771ad422
karleighponnock <karleighponnock@gmail.com>
2020-08-10 19:27:35 -0400

    classes added to increment button

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -9,7 +9,7 @@ class Counter extends Component {
     return (
       <>
         <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button>Increment</button>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </>
     );
   }
