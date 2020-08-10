a0f47a3c8555391734288b4f869db70175ec2fc1
karleighponnock <karleighponnock@gmail.com>
2020-08-10 19:54:02 -0400

    mapping over tags array

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,27 +2,28 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
-
 
   render() {
     return (
       <>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>{this.state.tags.map(tag => <li>{tag}</li>)}</ul>
       </>
     );
   }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
   formatCount() {
-      const{ count } = this.state;
+    const { count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
