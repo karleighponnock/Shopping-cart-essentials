d2c59b45f30f93c815f11c8c92262822c9c71424
karleighponnock <karleighponnock@gmail.com>
2020-08-10 19:22:07 -0400

    random image generated practice

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,20 +2,23 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
+    imgUrl: "https://picsum.photos/200"
   };
 
   render() {
     return (
       <>
-        <span>{this.state.count}</span>
+      <img src={this.state.imgUrl} alt="" />
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </>
     );
   }
 
   formatCount() {
-    return this.state.count === 0 ? "Zero" : this.state.count;
+      const{ count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
