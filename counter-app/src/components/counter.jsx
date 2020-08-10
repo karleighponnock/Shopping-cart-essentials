06943ed3ad0a43b1e5ea9d4ddf439217494d9b93
karleighponnock <karleighponnock@gmail.com>
2020-08-10 19:57:30 -0400

    count and button removed

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -9,22 +9,9 @@ class Counter extends Component {
   render() {
     return (
       <>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
         <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
       </>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
