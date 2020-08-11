11157a671b7378c718254f302f90d812b06fa879
karleighponnock <karleighponnock@gmail.com>
2020-08-10 21:07:08 -0400

    value from initial state is passed down to render.

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -7,13 +7,14 @@ class Counters extends Component {
           {id:1, value: 0},
           {id:2, value: 0},
           {id:3, value: 0},
-          {id:4, value: 0},
+          {id:4, value: 4},
       ]
   };
   render() {
     return (
       <div>
-      {this.state.counters.map(counter => <Counter key={counter.id} />)}
+      {this.state.counters.map(counter => 
+      <Counter key={counter.id} value={counter.value} selected={true} />)}
       </div>
     );
   }
