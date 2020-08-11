6b9ce360ce387506c065b8648ba233ebd31cd0f1
karleighponnock <karleighponnock@gmail.com>
2020-08-10 20:01:30 -0400

    render there are no tags if tags===0 or display tags

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,15 +3,23 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: [],
   };
 
-  render() {
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
     return (
-      <>
-        <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-      </>
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
+  }
+
+  render() {
+    return <>{this.renderTags()}</>;
   }
 }
 
