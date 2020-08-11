0dd5a3f15dda8dc27a8136f2048a544dafd108eb
karleighponnock <karleighponnock@gmail.com>
2020-08-10 21:12:16 -0400

    Titles added to each counter

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: this.props.value
+    value: this.props.value
   };
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count+1 })
+    this.setState({ count: this.state.value+1 })
   }
 
   render() {
     return (
       <>
+      {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button onClick={this.handleIncrement} 
         className="btn btn-secondary btn-sm">Increment</button>
@@ -21,12 +22,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -3,18 +3,21 @@ import Counter from "./counter";
 
 class Counters extends Component {
   state = {
-      counters: [
-          {id:1, value: 0},
-          {id:2, value: 0},
-          {id:3, value: 0},
-          {id:4, value: 4},
-      ]
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
   };
   render() {
     return (
       <div>
-      {this.state.counters.map(counter => 
-      <Counter key={counter.id} value={counter.value} selected={true} />)}
+        {this.state.counters.map((counter) => (
+          <Counter key={counter.id} value={counter.value}>
+            <h4>Title</h4>
+          </Counter>
+        ))}
       </div>
     );
   }
