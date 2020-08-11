7a30bd3b8011abb9884cb61daf26628169f474ad
karleighponnock <karleighponnock@gmail.com>
2020-08-10 20:08:05 -0400

    code reverted back to counter and button

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,24 +2,27 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: [],
+    count: 0
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
+  render() {
     return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
+      <>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </>
     );
   }
 
-  render() {
-    return <>{this.renderTags()}</>;
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
